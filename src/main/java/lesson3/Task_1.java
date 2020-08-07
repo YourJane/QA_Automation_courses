cee57272b32786cffd3447a4507ad0673ef32f0d
Jane <janerubtsova@gmail.com>
2020-08-07 21:48:00 +0100

    lesson3: tasks1-5

---
+++ src/main/java/lesson3/Task_1.java
@@ -0,0 +1,17 @@
+package lesson3;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        System.out.println("Minimal integer is: " + min(8, 10, 16));
+    }
+
+    public static int min(int usersFirstInt, int usersSecondInt, int usersThirdInt) {
+        if (usersFirstInt >= usersSecondInt && usersThirdInt >= usersSecondInt) {
+            return usersSecondInt;
+        } else if (usersFirstInt <= usersSecondInt && usersFirstInt <= usersThirdInt) {
+            return usersFirstInt;
+        } else {
+            return usersThirdInt;
+        }
+    }
+}
---
+++ src/main/java/lesson3/Task_4.java
@@ -0,0 +1,29 @@
+package lesson3;
+
+public class Task_4 {
+    public static void main(String[] args) {
+
+        String usersName = "Jane" + " ";
+        String joinedUsersName = "";
+        int countName = 0;
+        int countLines = 0;
+
+        do {
+            joinedUsersName += usersName;
+//            joinedUsersName += " ";
+            countName ++;
+        } while (countName != 10);
+
+        do {
+            System.out.println(joinedUsersName);
+            countLines ++;
+        } while (countLines != 5);
+
+//        Alternative solution using one for and Collection:
+//        do {
+//            String joinedUsersName = String.join(" ", Collections.nCopies(10, usersName));
+//            System.out.println(joinedUsersName);
+//        } while (countLines != 5);
+
+    }
+}
